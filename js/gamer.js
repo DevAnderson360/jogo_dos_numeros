4fabe3f5d4a26e7b2026cce4fc4b15a694a8e88d
Anderson Silverio <nogueirasilverio@gmail.com>
2020-08-10 19:20:22 -0400

    js && css: adicionando mais numeros

--- js/gamer.js
+++ js/gamer.js
@@ -1,3 +1,108 @@
+var numbers = [
+"zero",
+"um",
+"dois",
+"três",
+"quatro",
+"cinco",
+"seis",
+"sete",
+"oito",
+"nove",
+"dez",
+"onze",
+"doze",
+"treze",
+"quatorze",
+"quinze",
+"dezesseis",
+"dezessete",
+"dezoito",
+"dezenove",
+"vinte",
+"vinte e um",
+"vinte e dois",
+"vinte e três",
+"vinte e quatro",
+"vinte e cinco",
+"vinte e seis",
+"vinte e sete",
+"vinte e oito",
+"vinte e nove",
+"trinta",
+"trinta e um",
+"trinta e dois",
+"trinta e três",
+"trinta e quatro",
+"trinta e cinco",
+"trinta e seis",
+"trinta e sete",
+"trinta e oito",
+"trinta e nove",
+"quarenta",
+"quarenta e um",
+"quarenta e dois",
+"quarenta e três",
+"quarenta e quatro",
+"quarenta e cinco",
+"quarenta e seis",
+"quarenta e sete",
+"quarenta e oito",
+"quarenta e nove",
+"cinquenta",
+"cinquenta e um",
+"cinquenta e dois",
+"cinquenta e três",
+"cinquenta e quatro",
+"cinquenta e cinco",
+"cinquenta e seis",
+"cinquenta e sete",
+"cinquenta e oito",
+"cinquenta e nove",
+"sessenta",
+"sessenta e um",
+"sessenta e dois",
+"sessenta e três",
+"sessenta e quatro",
+"sessenta e cinco",
+"sessenta e seis",
+"sessenta e sete",
+"sessenta e oito",
+"sessenta e nove",
+"setenta",
+"setenta e um",
+"setenta e dois",
+"setenta e três",
+"setenta e quatro",
+"setenta e cinco",
+"setenta e seis",
+"setenta e sete",
+"setenta e oito",
+"setenta e nove",
+"oitenta",
+"oitenta e um",
+"oitenta e dois",
+"oitenta e três",
+"oitenta e quatro",
+"oitenta e cinco",
+"oitenta e seis",
+"oitenta e sete",
+"oitenta e oito",
+"oitenta e nove",
+"noventa",
+"noventa e um",
+"noventa e dois",
+"noventa e três",
+"noventa e quatro",
+"noventa e cinco",
+"noventa e seis",
+"noventa e sete",
+"noventa e oito",
+"noventa e nove",
+"cem"];
+
+
+
 let pontos = 0;
 let erros  = 0;
 
@@ -31,15 +136,8 @@ function setDroppable(select){
 
             let   ui_num = $(ui)[0].draggable.attr('key')
 
-            let msg = "Acertou"; 
-
             let status = this_num === ui_num ? true: false;
-
-            if(!status)
-            	msg = "Errou"
             
-            console.log(msg)
-
             if(status){
             	$(this).droppable( "option", "disabled", true );
             	$(this).css("background-color", "#92FF36");
@@ -68,7 +166,6 @@ function setErros()
 }
 
 //-----
-var numbers = ["Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "Quatorze", "Quinze"];
 
 //setar numeros
 function set(amount){
@@ -97,9 +194,9 @@ function renderNumber(n){
 
 function renderNumerals(nums){
 	nums.map(function(n){
-    	$("#numerals").append(`<div class="blockChildn" key="${n}" id="${numbers[n]}"><div class="container">${numbers[n]}</div></div>`)
+    	$("#numerals").append(`<div class="blockChildn" key="${n}" id="n_${n}"><div class="container">${numbers[n]}</div></div>`)
 		
-		setDroppable(`#${numbers[n]}`)
+		setDroppable(`#n_${n}`)
 
     });
 }
